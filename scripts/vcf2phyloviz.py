1509c9fbfd4390f7e39bfee774d50f74f0330170
Nick Loman <n.j.loman@bham.ac.uk>
2013-07-15 16:53:04 +0100

    little script for calculating mixture stats in VCF files

--- scripts/vcf2phyloviz.py
+++ scripts/vcf2phyloviz.py
@@ -40,40 +40,56 @@ def go(args, options):
 	nocalls_list = []
 
 	vcf_reader = vcf.Reader(open(args[0]), "r")
-	for record in vcf_reader:
+	vcf_records = list(vcf_reader)
+
+	for record in vcf_records:
 		for sample in record.samples:
-			if sample.gt_bases is None:#
+			if sample.gt_bases is None:
 				nocalls[sample.sample] += 1
 
 				nocalls_by_rec["%s-%s" % (record.CHROM, record.POS)].append(sample)
 
 				nocalls_list.append([record.CHROM, record.POS, record.REF, record.ALT, sample.sample, sample.data.SDP])
 
-		bases = [sample.gt_bases for sample in record.samples if sample.gt_bases is not None]
+	ignore_samples = []
+	total_records = float(len(vcf_records))
+	for sample, num_nocalls in nocalls.iteritems():
+		perc = float(num_nocalls) / total_records * 100
+		if perc > options.percent_nocalls:
+			ignore_samples.append(sample)
+
+	print "Samples to ignore: %s" % (", ".join(ignore_samples))
+
+	for record in vcf_records:
+		samples_to_use = [sample for sample in record.samples if sample.sample not in ignore_samples]
+		bases = [sample.gt_bases for sample in samples_to_use if sample.gt_bases is not None]
 	
-		if len(bases) != len(record.samples):
+		if len(bases) != len(samples_to_use):
 			continue
 
-		incorrect_lengths = [sample for sample in record.samples if len(sample.gt_bases.split("/")[0]) != 1]
+		incorrect_lengths = [sample for sample in samples_to_use if len(sample.gt_bases.split("/")[0]) != 1]
 
 		if incorrect_lengths: ## e.g. indels
 			continue	
 
-		for sample in record.samples:
+		for sample in samples_to_use:
 			alleles[sample.sample] += sample.gt_bases.split("/")[0]
-	
+
+# unique allele number	
 	unique_alleles = set(alleles.values())
 	allele_lookup = dict([(allele, n+1) for n, allele in enumerate(unique_alleles)])
 
-	print metadata
+# founder
+	allele_lookup = {}
+	for sample, genotype in alleles.iteritems():
+		if genotype not in allele_lookup:
+			allele_lookup[genotype] = sample
 
 	print >>fh_samples, "Sample\tProfile\t",
 	print >>fh_samples, "\t".join(metadata_columns)
 	for sample, genotype in alleles.iteritems():
 		print >>fh_samples, "%s\t%s\t" % (sample, allele_lookup[genotype]) ,
-		print sample
 		for sample_metadata in metadata:
-			print "  " + sample_metadata['Sample']
 			if sample_metadata['Sample'] == sample:
 				print >>fh_samples, "\t".join([sample_metadata[key] for key in metadata_columns if key != 'Sample']) ,
 		print >>fh_samples
@@ -84,14 +100,14 @@ def go(args, options):
 	for sample, value in sorted(nocalls.iteritems(), key=operator.itemgetter(1)):
 		print "%s => %s" % ( sample, value )
 
-	for loc, samplelist in sorted(nocalls_by_rec.iteritems(), key=operator.itemgetter(1)):
-		print "%s: " % (loc,)
- 		for sample in samplelist:
-			print "   %s" % (sample)
+#	for loc, samplelist in sorted(nocalls_by_rec.iteritems(), key=operator.itemgetter(1)):
+#		print "%s: " % (loc,)
+# 		for sample in samplelist:
+#			print "   %s" % (sample)
 
 	if options.nocallsfile:
 		nocall_fh = open(options.nocallsfile, "w")
-		print "CHROM\tPOS\tREF\tALT\tSample\tSDP"
+		print >>nocall_fh, "CHROM\tPOS\tREF\tALT\tSample\tSDP"
 		for nocall in nocalls_list:
 			print >>nocall_fh, "\t".join([str(s) for s in nocall])
 		nocall_fh.close()
@@ -107,6 +123,8 @@ def main():
 					  help="output alleles file to ALLELESFILE (default: <vcffile>_alleles.txt")
         parser.add_option("-n", "--nocalls", dest="nocallsfile",
                                           help="output alleles file to NOCALLSFILE (default: <vcffile>_nocalls.txt")
+	parser.add_option("-p", "--percentage_nocalls", dest="percent_nocalls", type="float", default=0.0,
+					  help="do not include samples with greater than <percentnocall> no calls")
 	parser.add_option("-v", "--verbose",
 					  action="store_true", dest="verbose")
 	parser.add_option("-q", "--quiet",
---
+++ scripts/vcfstats.py
@@ -0,0 +1,17 @@
+import vcf
+import sys
+from collections import Counter, defaultdict
+
+counts = defaultdict(Counter)
+
+vcf_reader = vcf.Reader(open(sys.argv[1], 'r'))
+for record in vcf_reader:
+	for sample in record.samples:
+		counts[sample.sample].update([sample['GT']])
+
+print "Sample\tHET\tHOM"
+for sample, counts in counts.iteritems():
+	print sample,
+	for gt in ['0/1', '1/1']:
+		print "\t" + str(counts[gt]),
+	print 
